87c5d985f8fb78c21b1eb9bfcfa2011c797711bd
shaikh-mohd-fahad <fahadazmilko@gmail.com>
2025-01-07 00:00:51 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Hero from "./components/Hero.jsx"
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
+      </main>
     </>
   )
 }
